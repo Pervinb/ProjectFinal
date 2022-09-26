65ffc710e49a59a75a4fea6bbeb4478e2a391065
parvin <bagirovp@gmail.com>
2022-09-26 23:24:48 +0400

    second commit

--- js/header.js
+++ js/header.js
@@ -1,6 +1,7 @@
 const h3 = document.getElementById('btn');
 const list = document.getElementById('list');
 
+
 list.style.display = "none";
 
 h3.addEventListener("click", (event) =>{
@@ -10,3 +11,20 @@ h3.addEventListener("click", (event) =>{
         list.style.display = "none"
     }
 })
+
+
+
+$(".owl-carousel").owlCarousel({
+    // loop: true,
+    // dots: true,
+    items: 5,
+    nav: true,
+    // center:true,
+    // responsiveClass: true,
+    margin:25,
+    navText:["<button><i class='fa fa-arrow-left'></i></button>" ,"<button><i class='fa fa-arrow-right'></i></button>"],
+});
+
+ 
+   
+
